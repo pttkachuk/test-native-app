c8ef93aa0ae0ee8df3131429c161193fa3efc4e8
pttkachuk <petrotkachuk6@gmail.com>
2024-01-16 15:12:28 +0100

    add initialScreen to main navigation

--- TestBollaApp/App.js
+++ TestBollaApp/App.js
@@ -7,6 +7,7 @@ import 'react-native-gesture-handler';
 import HomeScreen from './Screens/HomeScreen';
 import CameraScreen from './Screens/CameraScreen';
 import ReportScreen from './Screens/ReportScreen';
+import InitialScreen from './Screens/InitialScreen';
 
 const MainStack = createStackNavigator();
 
@@ -14,9 +15,22 @@ export default function App() {
   return (
     <NavigationContainer>
       <MainStack.Navigator initialRouteName='Home'>
-        <MainStack.Screen name='Home' component={HomeScreen} options={{ title: 'Pagina principale' }} />
-        <MainStack.Screen name='Report' component={ReportScreen} />
-        <MainStack.Screen name='Camera' component={CameraScreen} />
+        <MainStack.Screen name='Home' component={HomeScreen} options={{
+          title: 'Pagina principale', headerTitleAlign: 'center', headerStyle: {
+            backgroundColor: '#073C85',
+          }, headerTintColor: '#fff', headerShown: false,
+        }} />
+        <MainStack.Screen name='Report' component={ReportScreen} options={{
+          headerTitleAlign: 'center', headerStyle: {
+            backgroundColor: '#073C85',
+          }, headerTintColor: '#fff'
+        }} />
+        <MainStack.Screen name='Camera' component={CameraScreen} options={{
+          headerTitleAlign: 'center', headerStyle: {
+            backgroundColor: '#073C85',
+          }, headerTintColor: '#fff'
+        }} />
+        <MainStack.Screen name='Main' component={InitialScreen} options={{}} />
       </MainStack.Navigator>
 
     </NavigationContainer>
