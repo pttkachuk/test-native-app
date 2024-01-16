9704ac8e639f42320fd063e34351efe13e89d416
pttkachuk <petrotkachuk6@gmail.com>
2024-01-16 17:06:37 +0100

    working on

--- TestBollaApp/Screens/InitialScreen.jsx
+++ TestBollaApp/Screens/InitialScreen.jsx
@@ -1,11 +1,21 @@
+import { MaterialIcons } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 const InitialScreen = () => {
-  //console.log("i am here at Main screen");
+  const navigation = useNavigation();
+  const toCamera = () => {
+    navigation.navigate("Camera");
+    console.log("to Camera");
+  };
   return (
     <View style={styles.container}>
-      <Text>Initial Screen</Text>
+      <Text style={styles.userName}>Nome Cognome</Text>
+      <TouchableOpacity style={styles.addPhotoBtn} onPress={toCamera}>
+        <MaterialIcons name="add" size={90} color="#fff" />
+      </TouchableOpacity>
+      <Text style={styles.description}>Premi "Piu" per scattare la foto</Text>
     </View>
   );
 };
@@ -14,7 +24,28 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
+    //justifyContent: "center",
+    backgroundColor: "#fff",
+  },
+  userName: {
+    marginTop: 20,
+    fontFamily: "Fira-Sans-Regular",
+    fontSize: 20,
+  },
+  addPhotoBtn: {
+    marginTop: "50%",
+    marginBottom: "15%",
+    width: 180,
+    height: 180,
+    borderRadius: 90,
+    backgroundColor: "#073C85",
     justifyContent: "center",
+    alignItems: "center",
+    elevation: 35,
+  },
+  description: {
+    fontFamily: "Fira-Sans-Light",
+    fontSize: 14,
   },
 });
 
