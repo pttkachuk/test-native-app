99c0f1c5730e1647375109b208590e41acae234e
pttkachuk <petrotkachuk6@gmail.com>
2024-01-16 12:55:43 +0100

    adding navigation

--- TestBollaApp/App.js
+++ TestBollaApp/App.js
@@ -1,12 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import 'react-native-gesture-handler';
+
+import HomeScreen from './Screens/HomeScreen';
+import CameraScreen from './Screens/CameraScreen';
+import ReportScreen from './Screens/ReportScreen';
+
+const MainStack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <MainStack.Navigator initialRouteName='Home'>
+        <MainStack.Screen name='Home' component={HomeScreen} options={{ title: 'Pagina principale' }} />
+        <MainStack.Screen name='Report' component={ReportScreen} />
+        <MainStack.Screen name='Camera' component={CameraScreen} />
+      </MainStack.Navigator>
+
+    </NavigationContainer>
   );
 }
 
