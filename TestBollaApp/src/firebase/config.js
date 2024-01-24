264cf9cfd159e5d04b0eee07b76cacd5fbf90c42
pttkachuk <petrotkachuk6@gmail.com>
2024-01-24 15:50:17 +0100

    create confog.js

---
+++ TestBollaApp/src/firebase/config.js
@@ -0,0 +1,33 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+//import { getFirestore } from 'firebase/firestore';
+//import { getStorage } from 'firebase/storage';
+
+const firebaseConfig = {
+    // apiKey: ' AIzaSyBJGpJR5qyXcPwZUK-U5CeYSyzNBrsPx-o ',
+    // authDomain: 'technebollaapp.firebaseapp.com',
+    // databaseURL: '',
+    // projectId: 'technebollaapp',
+    // storageBucket: 'technebollaapp.appspot.com',
+    // messagingSenderId: '598287086773 ',
+    // appId: 'app-id',
+    // measurementId: 'G-measurement-id',
+    apiKey: "AIzaSyBJGpJR5qyXcPwZUK-U5CeYSyzNBrsPx-o",
+    authDomain: "technebollaapp.firebaseapp.com",
+    projectId: "technebollaapp",
+    storageBucket: "technebollaapp.appspot.com",
+    messagingSenderId: "598287086773",
+    appId: "1:598287086773:web:45c8167ffb976d5bedbafc",
+    measurementId: "G-NVTPXEE1X6"
+
+};
+
+const app = initializeApp(firebaseConfig);
+
+//export const auth = getAuth(app);
+
+export const auth = initializeAuth(app, {
+    persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+});
