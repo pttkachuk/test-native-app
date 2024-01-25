81b4b8769f7ec33326bc47881e9bc97543839f84
pttkachuk <petrotkachuk6@gmail.com>
2024-01-25 16:56:38 +0100

    add styles to login screen

--- TestBollaApp/Screens/LoginScreenFolder/LoginScreenStyle.js
+++ TestBollaApp/Screens/LoginScreenFolder/LoginScreenStyle.js
@@ -3,21 +3,46 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
+        padding: 15,
+        backgroundColor: '#fff',
+        //justifyContent: 'center',
     },
-    button: {
-        width: 150,
-        height: 70,
-        padding: 25,
-        marginTop: 40,
-        backgroundColor: '#073C85'
-
+    inputsContainer: {
+        display: 'flex',
+        flexDirection: 'column',
     },
-    text: {
-        textAlign: 'center',
-        color: '#fff',
+    input: {
+        width: "100%",
+        height: 50,
         fontFamily: "Fira-Sans-Light",
+        paddingLeft: 15,
+        backgroundColor: "transparent",
+        borderRadius: 8,
+        //marginBottom: 45,
+    },
+    btn: {
+        backgroundColor: "#073C85",
+        paddingTop: 13.5,
+        paddingBottom: 13.5,
+        borderRadius: 8,
+    },
+    btnText: {
+        textAlign: 'center',
+        color: "#fff",
+        fontFamily: "Fira-Sans-Medium",
         fontSize: 14,
-    }
+    },
+    photoInContainer: {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: 300,
+        backgroundColor: "transparent",
+        borderRadius: 8,
+        borderWidth: 1,
+        borderColor: "#D3D3D3",
+        marginBottom: 15,
+        overflow: "hidden",
+    },
 });
