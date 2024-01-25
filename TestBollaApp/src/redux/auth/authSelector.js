212235451c69346e045f300120b8f354cea4751d
pttkachuk <petrotkachuk6@gmail.com>
2024-01-25 16:57:00 +0100

    create authSelector

---
+++ TestBollaApp/src/redux/auth/authSelector.js
@@ -0,0 +1,3 @@
+export const selectEmail = (state) => state.auth.user.email;
+export const selectPassword = (state) => state.auth.user.password;
+export const selectUserName = (state) => state.auth.user.userName;
