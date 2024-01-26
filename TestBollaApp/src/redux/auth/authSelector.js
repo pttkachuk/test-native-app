3a951460cce65395493c7ca0864d975c2f75fc73
pttkachuk <petrotkachuk6@gmail.com>
2024-01-26 09:55:18 +0100

    add selectIsLoggedIn

--- TestBollaApp/src/redux/auth/authSelector.js
+++ TestBollaApp/src/redux/auth/authSelector.js
@@ -1,3 +1,4 @@
 export const selectEmail = (state) => state.auth.user.email;
 export const selectPassword = (state) => state.auth.user.password;
 export const selectUserName = (state) => state.auth.user.userName;
+export const selectIsLoggedIn = (state) => state.auth.isLoggedIn;
