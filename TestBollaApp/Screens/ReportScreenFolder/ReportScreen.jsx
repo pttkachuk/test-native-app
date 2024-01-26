2f48ee4e5eb4011d21268dbd1195c984e91bd10b
pttkachuk <petrotkachuk6@gmail.com>
2024-01-26 12:28:35 +0100

    upd mail template

--- TestBollaApp/Screens/ReportScreenFolder/ReportScreen.jsx
+++ TestBollaApp/Screens/ReportScreenFolder/ReportScreen.jsx
@@ -54,7 +54,7 @@ const ReportScreen = () => {
   const sendMail = async () => {
     try {
       const mailBody = `Buongiorno.\n\n Invio la bolla con numero di commessa: ${code}.\n\n ${
-        !userName ? "" : `Distinti saluti, ${userName}.`
+        !userName ? "" : `Distinti saluti.\n\n ${userName}, Techne S.p.A. .`
       }\n\n`;
       await MailComposer.composeAsync({
         recipients: ["petrotkachuk6@gmail.com"],
