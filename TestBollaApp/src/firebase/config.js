9fed17b8bbe3ea2c99b83bc9eb01dc98605ebbe1
pttkachuk <petrotkachuk6@gmail.com>
2024-01-26 15:22:06 +0100

    reinstalling firebase

---
+++ TestBollaApp/src/firebase/config.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBJGpJR5qyXcPwZUK-U5CeYSyzNBrsPx-o",
+    authDomain: "technebollaapp.firebaseapp.com",
+    projectId: "technebollaapp",
+    storageBucket: "technebollaapp.appspot.com",
+    messagingSenderId: "598287086773",
+    appId: "1:598287086773:web:45c8167ffb976d5bedbafc",
+    measurementId: "G-NVTPXEE1X6"
+};
+
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
