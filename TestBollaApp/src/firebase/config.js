af94f9711029581b121ba38aed31c62981dd2a91
pttkachuk <petrotkachuk6@gmail.com>
2024-01-26 15:53:19 +0100

    working on firebase

--- TestBollaApp/src/firebase/config.js
+++ TestBollaApp/src/firebase/config.js
@@ -2,10 +2,7 @@
 
 import { initializeApp } from "firebase/app";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 
 const firebaseConfig = {
     apiKey: "AIzaSyBJGpJR5qyXcPwZUK-U5CeYSyzNBrsPx-o",
