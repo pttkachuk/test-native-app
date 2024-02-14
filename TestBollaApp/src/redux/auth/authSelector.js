8ec695a7e18f473ff86b8550dde7b4fdc72c4072
pttkachuk <petrotkachuk6@gmail.com>
2024-02-14 11:41:41 +0100

    Update authSelector.js

--- TestBollaApp/src/redux/auth/authSelector.js
+++ TestBollaApp/src/redux/auth/authSelector.js
@@ -1,4 +1,4 @@
-export const selectEmail = (state) => state.auth.user.email;
+export const selectLogin = (state) => state.auth.user.login;
 export const selectPassword = (state) => state.auth.user.password;
 export const selectUserName = (state) => state.auth.user.userName;
 export const selectIsLoggedIn = (state) => state.auth.isLoggedIn;
