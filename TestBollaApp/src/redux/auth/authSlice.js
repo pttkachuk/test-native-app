3665c8383e39e4f71433381605a5d54226714777
pttkachuk <petrotkachuk6@gmail.com>
2024-02-14 16:38:15 +0100

    Update authSlice.js

--- TestBollaApp/src/redux/auth/authSlice.js
+++ TestBollaApp/src/redux/auth/authSlice.js
@@ -2,8 +2,10 @@ import { createSlice } from "@reduxjs/toolkit";
 import { loginUserThunk, logoutUserThunk } from "./authThunk";
 
 const initialState = {
-    user: { login: '', password: '', userName: '', },
+    user: { login: '', userName: '' },
     token: null,
+    isLoading: false,
+    error: null,
 };
 console.log('initialstate token:', initialState.token);
 
@@ -12,16 +14,35 @@ export const authSlice = createSlice({
     initialState,
     extraReducers: (builder) => {
         builder
+            //-------------------- LOGIN ---------------------------
+            .addCase(loginUserThunk.pending, (state) => {
+                state.isLoading = true;
+                state.error = null;
+            })
             .addCase(loginUserThunk.fulfilled, (state, payload) => {
                 console.log(payload.token);
+                state.isLoading = false;
                 state.user.login = payload.login;
-                state.user.password = payload.password;
-                state.user.userName = payload.userName;
+                state.user.userName = payload.name;
                 state.token = payload.token;
             })
+            .addCase(loginUserThunk.rejected, (state, payload) => {
+                state.isLoading = false;
+                state.error = payload;
+            })
+            //-------------------- LOGOUT ---------------------------
+            .addCase(logoutUserThunk.pending, (state) => {
+                state.isLoading = true;
+                state.error = null;
+            })
             .addCase(logoutUserThunk.fulfilled, (state) => {
-                state.user = { login: '', password: '', userName: '' };
+                state.isLoading = false;
+                state.user = { login: '', userName: '' };
                 state.token = null;
+            })
+            .addCase(logoutUserThunk.rejected, (state, payload) => {
+                state.isLoading = false;
+                state.error = payload;
             })
     }
 });
