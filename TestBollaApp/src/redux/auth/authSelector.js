1bc00eb548877225aaa3b38941ca2f91591010a2
pttkachuk <petrotkachuk6@gmail.com>
2024-02-15 09:33:46 +0100

    clear code

--- TestBollaApp/src/redux/auth/authSelector.js
+++ TestBollaApp/src/redux/auth/authSelector.js
@@ -1,5 +1,3 @@
 export const selectLogin = (state) => state.auth.user.login;
-//export const selectPassword = (state) => state.auth.user.password;
 export const selectUserName = (state) => state.auth.user.userName;
 export const selectIsAuth = (state) => state.auth.token;
-//export const selectIsLoggedIn = (state) => state.auth.isLoggedIn;
