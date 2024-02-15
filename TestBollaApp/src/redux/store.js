cc892475fe77571a8d0f3347cc0059ce5cf953f0
pttkachuk <petrotkachuk6@gmail.com>
2024-02-15 09:33:33 +0100

    Update store.js

--- TestBollaApp/src/redux/store.js
+++ TestBollaApp/src/redux/store.js
@@ -18,6 +18,7 @@ import { exitModalReducer } from './exitModal/exitModalSlice';
 const persistConfigAuth = {
     key: 'auth',
     storage: AsyncStorage,
+    whitelist: ['token'],
 };
 
 const persistAuthReducer = persistReducer(persistConfigAuth, authReducer);
