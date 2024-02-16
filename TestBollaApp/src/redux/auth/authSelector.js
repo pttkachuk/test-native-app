a72d625b1aa540a9244894a61f45a415dbcf6ab2
pttkachuk <petrotkachuk6@gmail.com>
2024-02-16 11:17:06 +0100

    upd selectors, upd name selector, token selector

--- TestBollaApp/src/redux/auth/authSelector.js
+++ TestBollaApp/src/redux/auth/authSelector.js
@@ -1,3 +1,4 @@
 export const selectLogin = (state) => state.auth.user.login;
-export const selectUserName = (state) => state.auth.user.userName;
-export const selectIsAuth = (state) => state.auth.token;
+export const selectUserName = (state) => state.auth.user.name;
+//export const selectIsAuth = (state) => state.auth.token;
+export const selectUserToken = (state) => state.auth.userToken;
