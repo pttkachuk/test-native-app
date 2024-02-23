c8fce729a47e86fc5414e14d3695a6a147299113
pttkachuk <petrotkachuk6@gmail.com>
2024-02-23 15:11:20 +0100

    upd login&logout thunk. add catch error verifications

--- TestBollaApp/src/redux/auth/authThunk.js
+++ TestBollaApp/src/redux/auth/authThunk.js
@@ -11,6 +11,15 @@ export const loginUserThunk = createAsyncThunk(
             //console.log('login data:', data);
             return data;
         } catch (error) {
+            // Personalizzazione del messaggio di errore
+            let errorMessage = "Si è verificato un errore durante il login. Riprova più tardi.";
+            if (error.response && error.response.status === 401) {
+                errorMessage = "Credenziali non valide. Verifica e riprova.";
+            } else if (error.response && error.response.status === 500) {
+                errorMessage = "Errore interno del server. Riprova più tardi.";
+            }
+
+            // Reject con il messaggio di errore personalizzato
             return thunkAPI.rejectWithValue(error.message);
         }
     }
@@ -41,6 +50,15 @@ export const logoutUserThunk = createAsyncThunk(
             console.log('logout THUNK data:', data);
             return data;
         } catch (error) {
+            // Personalizzazione del messaggio di errore
+            let errorMessage = "Si è verificato un errore durante la disconnessione. Riprova più tardi.";
+            if (error.response && error.response.status === 401) {
+                errorMessage = "Non sei autorizzato a effettuare questa operazione. Effettua nuovamente il login.";
+            } else if (error.response && error.response.status === 500) {
+                errorMessage = "Errore interno del server. Riprova più tardi.";
+            }
+
+            // Reject con il messaggio di errore personalizzato
             return thunkAPI.rejectWithValue(error.message);
         }
     }
