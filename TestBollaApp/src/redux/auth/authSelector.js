bdaae3280b9e8e0bc761e7e3d181fe38a6337b96
pttkachuk <petrotkachuk6@gmail.com>
2024-02-23 09:05:46 +0100

    upd selectors, add email selector

--- TestBollaApp/src/redux/auth/authSelector.js
+++ TestBollaApp/src/redux/auth/authSelector.js
@@ -1,3 +1,4 @@
 export const selectLogin = (state) => state.auth.user.login;
 export const selectUserName = (state) => state.auth.user.name;
+export const selectUserEmail = (state) => state.auth.user.email;
 export const selectUserToken = (state) => state.auth.userToken;
