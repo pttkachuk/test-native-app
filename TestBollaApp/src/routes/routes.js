65b06b6e66bfe0b0b05d873821285a35bd731ae4
pttkachuk <petrotkachuk6@gmail.com>
2024-03-04 12:53:32 +0100

    Create routes.js

---
+++ TestBollaApp/src/routes/routes.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { useSelector } from 'react-redux';
+
+const routes = () => {
+    return (
+        <div>routes</div>
+    )
+}
+
+export default routes
